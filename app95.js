34d67022440c9b81931d36928610d3842ac8fd10
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-30 11:04:52 +0530

    Add files via upload

---
+++ app95.js
@@ -0,0 +1,19 @@
+function getMin(nums) {
+    let min = nums.reduce((min,el) => {
+    if (min < el) {
+        return min;
+    } else {
+        return el;
+    }
+});
+
+    return min; 
+
+}
+
+
+
+let nums = [10,20,30,40,5];
+
+
+
